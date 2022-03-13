3dd54bf8073842c898b340b64713b14593fe3890
isselin28 <isselin.moektijono@gojek.com>
2022-03-13 20:58:33 +0800

    setup and add assets

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+    colors: {
+      blue: "#1B294B",
+      yellow: "#ffc82c",
+      "gray-dark": "#273444",
+      gray: "#8492a6",
+      "gray-light": "#d3dce6",
+      slate: "#e2e8f0",
+      white: "#ffffff",
+    },
+    backgroundImage: {
+      "gradient-radial": "radial-gradient(var(--gradient-color-stops))",
+    },
+  },
+  plugins: [],
+};
